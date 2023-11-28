fc50acaec9b537810067440c1c2e11060dcf4c74
Ta Van Dinh <dinhtv7@fpt.edu.vn>
2023-11-28 22:34:14 +0700

    update

--- view/addToCart.php
+++ view/addToCart.php
@@ -1,6 +1,12 @@
 <?php
 session_start();
 
+// Kiểm tra xem có tồn tại mảng giỏ hàng hay không.
+if (!isset($_SESSION['cart'])) {
+    // Nếu không có thì đi khởi tạo
+    $_SESSION['cart'] = [];
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lấy dữ liệu từ ajax đẩy lên
     $productId = $_POST['id'];
@@ -8,7 +14,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $productPrice = $_POST['price'];
 
     // Kiểm tra sản phẩm đã có trong giỏ hàng chưa
-    $index = array_search($productId, array_column($_SESSION['cart'], 'id'));
+    $index = false;
+    if (!empty($_SESSION['cart'])) {
+        $index = array_search($productId, array_column($_SESSION['cart'], 'id'));
+    }
+    
     // array_column() trích xuất một cột từ mảng giỏ hàng và trả về một mảng chứ giá trị của cột id
     if ($index !== false) {
         $_SESSION['cart'][$index]['quantity'] += 1;
@@ -21,6 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             'quantity' => 1
         ];
         $_SESSION['cart'][] = $product;
+        // var_dump($_SESSION['cart']);die;
     }
     // Trả về số lượng sản phẩm của giỏ hàng
     echo count($_SESSION['cart']);
